26f00ff0221107862111390efb6af90297e21df8
vikaskmr47 <vikaskmr047@gmail.com>
2022-06-17 22:37:31 +0530

    Committed on 17th June

---
+++ ADODemoTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace ADODemoTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string Expected = "Hello World!";
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+
+        }
+    }
+}
